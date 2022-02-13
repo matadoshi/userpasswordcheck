bc059b451e91c1b4efe9c1f21c6ed7f3a86e1d24
onursmlv <onurtis@code.edu.az>
2022-02-13 17:43:03 +0400

    Done Task/1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UserPasswordCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Username daxil edin");
+            string userid = Console.ReadLine();
+            User user1 = new User(userid);
+            Console.WriteLine("Parolunuzu daxil edin");
+
+            string userpsw = Console.ReadLine();
+
+            user1.Password = userpsw;
+
+            Console.WriteLine($"Istifadeci Yaradildi. Username:{user1.Name} Password:{user1.Password}");
+            
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserPasswordCheck
+{
+    class User
+    {
+
+        private string _username;
+        private string _password;
+        
+        public User(string userid)
+        {
+            Name = userid;
+           
+        }
+        public User(string userid,string userpsw) : this(userid)
+        {
+            Password = userpsw;
+        }
+        
+        public string Name
+        {
+            get{
+                
+                return _username;
+            }
+            set
+            {
+               
+                while (true)
+                {
+                    if (value.Length < 8)
+                    {
+                        Console.WriteLine("Uzunluq 8den az ola bilmez");
+                        value = Console.ReadLine();
+                    }
+                    else
+                    {
+                        _username = value;
+                        break;
+
+                    }
+                    
+
+                }
+                
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                while (true)
+                {
+
+                    bool checkDigit = false;
+                    bool checkLetter = false;
+                    bool checkUpper = false;
+
+
+
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        if (char.IsDigit(value[i]))
+                        {
+                            checkDigit = true;
+                        }
+                        if (char.IsLetter(value[i]))
+                        {
+                            checkLetter = true;
+
+                        }
+                        if (char.IsUpper(value[i]))
+                        {
+                            checkUpper = true;
+                        }
+
+
+                    }
+                    if (checkDigit && checkLetter && checkUpper)
+                    {
+                        _password = value;
+                        break;
+
+                    }
+                    else
+                    {
+
+                    }
+                    {
+
+                        Console.WriteLine("Sifreni Duzgun Daxil Edin");
+                        value = Console.ReadLine();
+
+                        
+                    }
+
+                }
+
+             
+            }
+
+
+
+
+        }
+
+
+
+    }
+}
